53dee579361f565d2fe0c40194da49b6e3af722c
Yoloyuanyi <1443205744@qq.com>
2023-07-19 16:35:08 +0800

    项目环境搭建

---
+++ src/main/java/com/sx/client_collection/ClientCollectionApplication.java
@@ -0,0 +1,14 @@
+package com.sx.client_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class ClientCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientCollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sx/client_collection/ClientCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sx.client_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
